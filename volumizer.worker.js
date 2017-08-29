b678a8f690ea501681d735e7c9853681b6023f0b
radishengine <radishengine@gmail.com>
2017-08-29 15:57:00 +0100

    Update volumizer.worker.js

--- volumizer.worker.js
+++ volumizer.worker.js
@@ -1,36 +1,46 @@
 
 self.onmessage = function onmessage(e) {
   var message = e.data;
-  switch (message.headline) {
-    case 'open-blob':
-      var url = URL.createObjectURL(new Blob([new Uint8Array(1024 * 1024)]));
-      fetch(url).then(function(r) {
-        var reader = r.body.getReader();
-        function nextChunk(chunk) {
-          if (chunk.done) {
-            URL.revokeObjectURL(url);
-            postMessage({
-              id: message.id,
-              headline: 'close',
-              final: true,
-              result: null,
-            });
-            return;
+  try {
+    switch (message.headline) {
+      case 'open-blob':
+        var url = URL.createObjectURL(new Blob([new Uint8Array(1024 * 1024)]));
+        fetch(url).then(function(r) {
+          var reader = r.body.getReader();
+          function nextChunk(chunk) {
+            if (chunk.done) {
+              URL.revokeObjectURL(url);
+              postMessage({
+                id: message.id,
+                headline: 'close',
+                final: true,
+                result: null,
+              });
+              return;
+            }
+            chunk = chunk.value;
+            console.log(chunk.length);
+            return reader.read().then(nextChunk);
           }
-          chunk = chunk.value;
-          console.log(chunk.length);
           return reader.read().then(nextChunk);
-        }
-        return reader.read().then(nextChunk);
-      });
-      break;
-    default:
-      postMessage({
-        id: message.id,
-        headline: 'problem',
-        final: true,
-        problem: 'unrecognized message headline: ' + message.headline,
-      });
-      break;
+        });
+        break;
+      default:
+        postMessage({
+          id: message.id,
+          headline: 'problem',
+          final: true,
+          problem: 'unrecognized message headline: ' + message.headline,
+        });
+        break;
+    }
+  }
+  catch (e) {
+    postMessage({
+      id: message.id,
+      headline: 'problem',
+      final: true,
+      problem: ''+e,
+    });
   }
 };
