e00c27b9feb3ddf555d2df26e5c92f8feb76169e
radishengine <radishengine@gmail.com>
2017-08-29 17:07:55 +0100

    Update volumizer.worker.js

--- volumizer.worker.js
+++ volumizer.worker.js
@@ -1,46 +1,47 @@
 
+importScripts(
+  'data.js');
+
+const handlers = {
+  'open-blob': function(message) {
+    var meg = new Uint8Array(1024 * 1024);
+    var blob = new Blob([
+      meg, meg, meg, meg, meg,
+      meg, meg, meg, meg, meg]);
+    function onChunk(chunk) {
+      console.log(chunk.length);
+    }
+    return data.streamBlob(onChunk, blob);
+  },
+};
+
 self.onmessage = function onmessage(e) {
   var message = e.data;
-  try {
-    switch (message.headline) {
-      case 'open-blob':
-        var url = URL.createObjectURL(new Blob([new Uint8Array(1024 * 1024)]));
-        fetch(url).then(function(r) {
-          var reader = r.body.getReader();
-          function nextChunk(chunk) {
-            if (chunk.done) {
-              URL.revokeObjectURL(url);
-              postMessage({
-                id: message.id,
-                headline: 'close',
-                final: true,
-                result: null,
-              });
-              return;
-            }
-            chunk = chunk.value;
-            console.log(chunk.length);
-            return reader.read().then(nextChunk);
-          }
-          return reader.read().then(nextChunk);
-        });
-        break;
-      default:
-        postMessage({
-          id: message.id,
-          headline: 'problem',
-          final: true,
-          problem: 'unrecognized message headline: ' + message.headline,
-        });
-        break;
-    }
-  }
-  catch (e) {
+  if (!(message.headline in handlers)) {
     postMessage({
       id: message.id,
       headline: 'problem',
       final: true,
-      problem: ''+e,
+      problem: 'unrecognized message headline: ' + message.headline,
     });
+    return;
   }
+  var handler = handlers[message.headline];
+  return handler(message).then(
+    function(result) {
+      postMessage({
+        id: message.id,
+        headline: 'complete',
+        final: true,
+        result: result,
+      });
+    },
+    function(reason) {
+      postMessage({
+        id: message.id,
+        headline: 'problem',
+        final: true,
+        problem: ''+e,
+      });
+    });
 };
