054870f5ba33591c041578f233ecbf6de5cce09c
radishengine <radishengine@gmail.com>
2017-08-29 17:31:09 +0100

    Update volumizer.worker.js

--- volumizer.worker.js
+++ volumizer.worker.js
@@ -2,7 +2,37 @@
 importScripts(
   'data.js');
 
-const handlers = {
+self.onmessage = function onmessage(e) {
+  onmessage.promiseChain = onmessage.promiseChain.then(function() {
+    var message = e.data;
+    var handled;
+    if (message.headline in onmessage.handlers) {
+      handled = onmessage.handlers[message.headline](message);
+    }
+    else {
+      handled = Promise.reject('unrecognized message headline: ' + message.headline);
+    }
+    return handled.then(
+      function(result) {
+        postMessage({
+          id: message.id,
+          headline: 'complete',
+          final: true,
+          result: result,
+        });
+      },
+      function(reason) {
+        postMessage({
+          id: message.id,
+          headline: 'problem',
+          final: true,
+          problem: ''+e,
+        });
+      });
+  });
+};
+
+onmessage.handlers = {
   'open-blob': function(message) {
     var meg = new Uint8Array(1024 * 1024);
     var blob = new Blob([
@@ -15,33 +45,4 @@ const handlers = {
   },
 };
 
-self.onmessage = function onmessage(e) {
-  var message = e.data;
-  if (!(message.headline in handlers)) {
-    postMessage({
-      id: message.id,
-      headline: 'problem',
-      final: true,
-      problem: 'unrecognized message headline: ' + message.headline,
-    });
-    return;
-  }
-  var handler = handlers[message.headline];
-  return handler(message).then(
-    function(result) {
-      postMessage({
-        id: message.id,
-        headline: 'complete',
-        final: true,
-        result: result,
-      });
-    },
-    function(reason) {
-      postMessage({
-        id: message.id,
-        headline: 'problem',
-        final: true,
-        problem: ''+e,
-      });
-    });
-};
+onmessage.promiseChain = Promise.resolve();
