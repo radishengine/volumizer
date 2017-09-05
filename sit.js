56eb5f90d215afeb4ee161b7c7ba89d3788d9f05
radishengine <radishengine@gmail.com>
2017-09-05 19:28:13 +0100

    Update sit.js

--- sit.js
+++ sit.js
@@ -4,7 +4,7 @@ if (!('data' in self)) importScripts('data.js');
 self.sit = {};
 
 sit.original = function original(id, cc, sectors) {
-  return cc.getBytes(data.sectorize(sectors, 0, 14)).then(function(bytes) {
+  return Promise.resolve(cc.getBytes(data.sectorize(sectors, 0, 14))).then(function(bytes) {
     if (bytes.sublen(0, 4).toByteString() !== 'SIT!' || bytes.sublen(10, 4) !== 'rLau') {
       return false;
     }
@@ -13,7 +13,7 @@ sit.original = function original(id, cc, sectors) {
 };
 
 sit.v5 = function v5(id, cc, sectors) {
-  return cc.getBytes(data.sectorize(sectors, 0, 80)).then(function(bytes) {
+  return Promise.resolve(cc.getBytes(data.sectorize(sectors, 0, 80))).then(function(bytes) {
     if (!/^StuffIt \(c\)1997-\d{4} Aladdin Systems, Inc\., http:\/\/www.aladdinsys.com\/StuffIt\/\r\n/.test(bytes.toByteString())) {
       return false;
     }
