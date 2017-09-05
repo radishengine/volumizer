4001545047a0e5bb003974e8a3225460cfe681c8
radishengine <radishengine@gmail.com>
2017-09-05 01:27:46 +0100

    Update VolumizerClient.js

--- VolumizerClient.js
+++ VolumizerClient.js
@@ -3,10 +3,12 @@ function VolumizerClient() {
   this.worker = new Worker('volumizer.worker.js');
   this.worker.tasks = {};
   this.worker.addEventListener('message', function downloadChecker(e) {
-    if (e.data.headline === 'download') {
+    var message = e.data;
+    if (typeof message === 'string') message = JSON.parse(message);
+    if (message.headline === 'download') {
       var link = document.createElement('A');
-      link.href = URL.createObjectURL(e.data.file);
-      link.setAttribute('download', e.data.file.name || 'file.dat');
+      link.href = URL.createObjectURL(message.file);
+      link.setAttribute('download', message.file.name || 'file.dat');
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
@@ -33,6 +35,7 @@ VolumizerClient.prototype = {
       } while (id in worker.tasks);
       function onmessage(e) {
         var message = e.data;
+        if (typeof message === 'string') message = JSON.parse(message);
         if (message.id !== id) return;
         if (message.headline === 'callback' && message.callback in callbacks) {
           callbacks[message.callback].apply(callbacks, message.args);
