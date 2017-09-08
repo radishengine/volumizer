77939f8e8bf3a9fa08106840253088a8ff840818
radishengine <radishengine@gmail.com>
2017-09-08 23:01:17 +0100

    Update sit.js

--- sit.js
+++ sit.js
@@ -521,14 +521,14 @@ sit.decode_mode3 = function decode_mode3(id, cc, sectors, outputLength) {
     
     function readBranch() {
       if (bitCount === 0) {
-        bitBuf |= input[input_i++] << bitCount;
+        bitBuf = (bitBuf << 8) | input[input_i++];
         bitCount += 8;
       }
       var isLeaf = bitBuf & 1;
       bitBuf >>>= 1; bitCount -= 1;
       if (isLeaf) {
         if (bitCount < 8) {
-          bitBuf |= input[input_i++] << bitCount;
+          bitBuf = (bitBuf << 8) | input[input_i++];
           bitCount += 8;
         }
         var leafValue = bitBuf & 0xff;
@@ -547,7 +547,7 @@ sit.decode_mode3 = function decode_mode3(id, cc, sectors, outputLength) {
       var branch = tree;
       while (typeof branch !== 'number') {
         if (bitCount === 0) {
-          bitBuf |= input[input_i++] << bitCount;
+          bitBuf = (bitBuf << 8) | input[input_i++];
           bitCount += 8;
         }
         branch = branch[bitBuf & 1];
