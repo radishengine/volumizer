13cd40396e09ef3ff1a9f13669501aff77d11bd9
radishengine <radishengine@gmail.com>
2017-09-11 12:39:31 +0100

    Update sit.js

--- sit.js
+++ sit.js
@@ -1303,26 +1303,16 @@ sit.decode_mode15 = function decode_mode15(id, cc, sectors, outputLength) {
     var bitBuf = 0, bitCount = 0;
     function bits(n) {
       while (bitCount < n) {
-        bitBuf |= input[input_i++] << bitCount;
-        bitCount += 8;
-      }
-      var v = bitBuf & ((1 << n) - 1);
-      bitBuf >>>= n;
-      bitCount -= n;
-      return v;
-      /*
-      while (bitCount < n) {
         bitBuf = (bitBuf << 8) | input[input_i++];
         bitCount += 8;
       }
       var v = bitBuf << (32 - bitCount) >>> (32 - n);
       bitCount -= n;
       return v;
-      */
     }
     const nbits = 26;
-    const one = 1 << nbits;
-    const half = one >>> 1;
+    const one = 1 << (nbits-1);
+    const half = 1 << (nbits-2);
     var model, code, range, symbolFreq = new Array(256);
     function arithinit() {
       range = one;
