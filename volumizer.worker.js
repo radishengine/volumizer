8f1519e0c11a2c9dac8810da6c8c68e079f8f31a
radishengine <radishengine@gmail.com>
2017-09-30 14:11:36 +0100

    Update volumizer.worker.js

--- volumizer.worker.js
+++ volumizer.worker.js
@@ -1,12 +1,19 @@
 
 importScripts('volumizer.js');
 
+volumizer.withTransaction(['workers'], 'readwrite', function(t) {
+  return new Promise(function(resolve, reject) {
+    t.objectStore('workers')
+      .add({createdAt:new Date()})
+      .onsuccess = function(e) {
+        resolve(e.result);
+      };
+  });
+})
+.then(function(id) {
+  self.postMessage('{"headline":"init", "id":' + id + '}');
+});
+
 self.onmessage = function onmessage(e) {
   var message = e.data;
-  switch (message.headline) {
-    case 'init':
-      self.name = message.name;
-      console.log(self.name);
-      break;
-  }
 };
