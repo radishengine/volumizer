35162c955f69c57378f601991dbd6fe11786ebf5
radishengine <radishengine@gmail.com>
2017-10-02 17:51:58 +0100

    Update volumizer.worker.js

--- volumizer.worker.js
+++ volumizer.worker.js
@@ -1,19 +1,13 @@
 
 importScripts('volumizer.js');
 
-volumizer.withTransaction(['workers'], 'readwrite', function(t) {
-  return new Promise(function(resolve, reject) {
-    t.objectStore('workers')
-      .add({createdAt:new Date()})
-      .onsuccess = function(e) {
-        resolve(e.result);
-      };
-  });
-})
-.then(function(id) {
-  self.postMessage('{"headline":"init", "id":' + id + '}');
+self.addEventListener('volumizer-task-unclaimed', function(e) {
+  var task = e.detail;
+  console.log('claiming task ' + task.id);
+  e.preventDefault();
 });
 
 self.onmessage = function onmessage(e) {
   var message = e.data;
+  volumizer.tryClaimTask();
 };
