bfb61841cd0c2b8b4b2bf961cb70937f21c1f72b
radishengine <radishengine@gmail.com>
2017-10-03 13:10:23 +0100

    Update volumizer.worker.js

--- volumizer.worker.js
+++ volumizer.worker.js
@@ -1,15 +1,25 @@
 
 importScripts('volumizer.js');
 
+var handlers = {
+  'prime-source': function(task) {
+    console.log(task.url);
+  },
+};
+
 self.addEventListener('volumizer-task-unclaimed', function(e) {
   var task = e.detail;
-  switch (task.operation) {
-    case 'prime-source':
-      console.log('claiming task ' + task.id + ' (' + task.operation + ')');
-      console.log(task);
-      e.preventDefault();
-      break;
-  }
+  if (!(task.operation in handlers)) return;
+  e.preventDefault();
+  console.log('claiming task:', task);
+  var handler = handlers[task.operation];
+  Promise.resolve(task).then(handler).then(
+    function() {
+      volumizer.update('tasks', task.id, {completedAt:new Date});
+    },
+    function(reason) {
+      volumizer.update('tasks', task.id, {worker:-2, failedAt:new Date, error:reason+''});
+    });
 });
 
 self.onmessage = function onmessage(e) {
